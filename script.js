38d7a10b8019d9f3010f9d6bce94f1392a405367
Nathan Akpawu <nakpawu@gmail.com>
2022-06-17 08:52:53 +0000

    highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -17,6 +18,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         // When guess is to high
     } else if (guess > secretNumber) {
         if (score > 1) {
